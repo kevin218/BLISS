ad771e78b9dce1d9a058695b6cb9e5d6c30dd06a
Carlos E. Munoz Romero <32044135+munozcar@users.noreply.github.com>
2018-07-05 16:27:49 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,5 +1,6 @@
 import BLISS as BLISS
 import matplotlib.pyplot as plt
+from scipy import spatial 
 
 y,x = 0,1
 dataDir = "/Users/cmunoz/Desktop/Research/GJ1214/data/group0_gsc.joblib.save"
@@ -11,13 +12,10 @@ ySigmaRange = 3
 points, fluxes = BLISS.extractData(dataDir)
 points, fluxes = BLISS.removeOutliers(points, fluxes, xSigmaRange, ySigmaRange)
 knots = BLISS.createGrid(points, xBinSize, yBinSize)
-knotTree = BLISS.spatial.KDTree(knots)
+knotTree = spatial.KDTree(knots)
 nearIndices = BLISS.nearestIndices(points, knotTree)
 normFactor = (1/xBinSize) * (1/yBinSize)
 
 interpolFluxes = BLISS.BLISS(points, fluxes, knots, nearIndices)
 
-plt.scatter([p[0] for p in points], [p[1] for p in points], c=interpolFluxes, s=0.1)
-plt.colorbar()
-plt.show()
 
