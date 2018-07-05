a355dd34f36a4c091bfe1e874bce7c07e7f7447b
Carlos E. Munoz Romero <32044135+munozcar@users.noreply.github.com>
2018-07-05 16:50:54 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -9,14 +9,15 @@ yBinSize = 0.01
 xSigmaRange = 3
 ySigmaRange = 3
 
-points, fluxes = BLISS.extractData(dataDir)
-points, fluxes = BLISS.removeOutliers(points, fluxes, xSigmaRange, ySigmaRange)
-knots = BLISS.createGrid(points, xBinSize, yBinSize)
+points, fluxes = extractData(dataDir)
+points, fluxes = removeOutliers(points, fluxes, xSigmaRange, ySigmaRange)
+knots = createGrid(points, xBinSize, yBinSize)
 knotTree = spatial.KDTree(knots)
-nearIndices = BLISS.nearestIndices(points, knotTree)
+nearIndices = nearestIndices(points, knotTree)
 normFactor = (1/xBinSize) * (1/yBinSize)
 
-interpolFluxes = BLISS.BLISS(points, fluxes, knots, nearIndices)
+interpolFluxes = BLISS(points, fluxes, knots, nearIndices, xBinSize=xBinSize, yBinSize = yBinSize, 
+                       normFactor=normFactor)
 
 plt.scatter([p[0] for p in points], [p[1] for p in points], s=0.1, c=interpolFluxes)
 plt.colorbar()
