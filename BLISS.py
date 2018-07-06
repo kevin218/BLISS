c803f217d39205211ac5381092db29a15ce78297
Jonathan Fraine <exowanderer@users.noreply.github.com>
2018-07-06 15:23:13 -0400

    put back to original -- using branch now

--- BLISS.py
+++ BLISS.py
@@ -7,13 +7,11 @@ y, x = 0, 1
 
 def extractData(file):
     group = joblib.load(file)
-    times = group[b'times'].flatten()
     xcenters = group[b'centers'][0, :, :, x].flatten()
     ycenters = group[b'centers'][0, :, :, y].flatten()
     fluxes = group[b'phots'][0, -1].flatten()
-    flux_errs = group[b'photserr'][0, -1].flatten()
-    # points = np.array([xcenters, ycenters])
-    return times, xcenter, ycenters, fluxes, flux_errs
+    points = np.array([xcenters, ycenters])
+    return points, fluxes
 
 def nearest(point, neighbors, tree):
     """
